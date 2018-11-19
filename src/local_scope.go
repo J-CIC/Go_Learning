46566971c415725a13f9be357095b4fa3d94b17b
CIC <791863347@qq.com>
2018-11-19 23:29:15 +0800

    add 3 tests

--- src/local_scope.go
+++ src/local_scope.go
@@ -3,14 +3,64 @@ package main
 var a = "G"
 
 func main() {
-	n()
-	m()
-	n()
+   n()
+   m()
+   n()
 }
 
 func n() { print(a) }
 
 func m() {
-	a := "O"
-	print(a)
+   a := "O"
+   print(a)
 }
+//GOG
+
+/*
+package main
+
+var a = "G"
+
+func main() {
+   n()
+   m()
+   n()
+}
+
+func n() {
+   print(a)
+}
+
+func m() {
+   a = "O"
+   print(a)
+}
+
+//GOO
+*/
+
+/*
+
+package main
+
+var a string
+
+func main() {
+   a = "G"
+   print(a)
+   f1()
+}
+
+func f1() {
+   a := "O"
+   print(a)
+   f2()
+}
+
+func f2() {
+   print(a)
+}
+
+GOG
+
+*/
